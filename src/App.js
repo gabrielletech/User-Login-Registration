3ae23cadec7a901b96037d9185baccd72b700ab6
Fadziso Gabrielle <fadzisondoro@icloud.com>
2021-10-02 01:15:25 +0200

    Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import LoginBtn from './components/LoginBtn';
 
 function App() {
   return (
-    <div>
-    
-    </div>
+    <LoginBtn />
   );
 }
 
---
+++ src/components/LoginBtn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginBtn = () => {
+    const { loginWithRedirect } = useAuth0();
+    return (
+        <button onClick={() => loginWithRedirect()}>
+            Log In
+        </button>
+    )
+}
+
+export default LoginBtn;

213c3cfde7c3e8f86c4b3b7650dcce2c9feab33e
Fadziso Gabrielle <fadzisondoro@icloud.com>
2021-10-03 09:28:43 +0200

    Added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoginForm = () => {
     return (
-        <div>
+        <div className="main">
             
         </div>
     )
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -9,10 +9,10 @@ const WelcomePage = () => {
             <h1 className="welcome-header display-4">Hello, please Log In or Sign Up</h1>
             <div className="buttons">
                         <button type="button" className="btn btn-outline-secondary btn-lg log-in">
-                            <Link to="/login">Log In</Link>
+                            <Link to="/login" className="login-btn">Log In</Link>
                         </button>
                         <button type="button" className="btn btn-outline-secondary btn-lg sign-up">
-                            <Link to="/signup">Sign Up</Link>
+                            <Link to="/signup" className="signup-btn">Sign Up</Link>
                         </button>
             </div>
         </div>

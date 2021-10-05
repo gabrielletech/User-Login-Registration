4926729f4e5f4f16fc7d1060141a0a1f6eb3df41
Fadziso Gabrielle <fadzisondoro@icloud.com>
2021-10-05 13:25:56 +0200

    Added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -39,6 +39,15 @@ const LoginForm = () => {
                                 </div>
                                 <input type="text" className="form-control" placeholder="Password" />
                             </div>
+                            <button type="button" className="btn btn-secondary btn-block login-sub">LOGIN</button>
+                            <div className="message">
+                                <div>
+                                    <input type="checkbox" /> Remember ME
+                                </div>
+                                <div>
+                                    <a href="/">Forgot your password</a>
+                                </div>
+                            </div>
                         </form>
                     </div>  
                 </div>  

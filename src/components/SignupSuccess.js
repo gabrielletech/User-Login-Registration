c87389e00b508f80828d5916e5f46f0ba105e95c
Fadziso Gabrielle <fadzisondoro@icloud.com>
2021-10-07 22:10:35 +0200

    Added

---
+++ src/components/SignupSuccess.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SignupSuccess = () => {
+    return (
+        <>
+          <div className="main">
+              <h1 className="signup-success">Sign Up Successful!</h1>
+              <h4 className="lead">Proceed to Log In</h4>
+              <div>
+                <button></button>
+              </div>
+          </div>  
+        </>
+    )
+}
+
+export default SignupSuccess;
